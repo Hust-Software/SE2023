df79213b92b344d85826f2b35ff86ebfd0d4449b
lichangkun2003 <1121974023@qq.com>
2023-04-25 18:30:37 +0800

    Create history.js

---
+++ Translate-master/pages/history/history.js
@@ -0,0 +1,20 @@
+// history.js
+const app = getApp()
+
+Page({
+  data: {
+    history: []
+  },
+
+  onShow: function() {
+    this.setData({
+      history: wx.getStorageSync('history')
+    })
+  },
+
+  onTapItem: function(e) {
+    wx.reLaunch({
+      url: `/pages/index/index?query=${e.currentTarget.dataset.query}`
+    })
+  },
+})
