187f6aa5d5a1ff853d7977280033fecee08e2bd8
张航天 <1665480280@qq.com>
2023-05-16 16:54:47 +0800

    Update index.js

--- Translate-master/pages/index/index.js
+++ Translate-master/pages/index/index.js
@@ -144,62 +144,60 @@ Page({
       sourceType: ['album', 'camera'],
       camera: 'back',
       success:(res) => {
-          //const tmpfilePath = res.tempFiles.tempFilePath
           console.log(res.tempFiles[0].tempFilePath)
           // 获取用户文件路径
         wx.showLoading({
           title: '正在识别',
         })
-
-        var that = this
         let imagePath = res.tempFiles[0].tempFilePath
-        var image
+        var image 
         fileManger.readFile({
           //路径
           filePath:imagePath,
-          //encoding :,
+          encoding :'base64',
           //转换的编码格式
           success: res => {
            image = res.data
-           console.log('encoding success'+image)
-          },
+           var salt = Date.now()
+           var cuid ='APICUID'
+           var mac = 'mac'
+           var imagedata = md5(image)
+           var sign = md5(`${appid}${imagedata}${salt.toString()}${cuid}${mac}${key}`)
+           console.log(`${appid}${imagedata}${salt.toString()}${cuid}${mac}${key}`)
+           wx.uploadFile({
+             url: 'https://fanyi-api.baidu.com/api/trans/sdk/picture',
+             filePath: imagePath,
+             name: '1.jpg',
+             header:{
+                 'content-type':'multipart/form-data'
+             },
+             formData: {
+               image,
+               from:'zh',
+               to:'en',
+               appid,
+               salt,
+               cuid,
+               mac,
+               version:3,
+               sign
+             },
+             success(res) {
+               console.log('上传成功',res.data)
+             },
+             fail(res) {
+               console.log(res)
+             },
+             complete() {
+               wx.hideLoading()
+             }
+           })
+         }
+       })
+      },
           fail(res) {
             console.error(res)
           }
-        })
-        let salt = Date.now()
-        let cuid ='APICUID'
-        let mac = 'mac'
-        let imagedata = md5(image)
-        let sign = md5(`${appid}${imagedata}${salt}${cuid}${mac}${key}`)
-        console.log(appid+imagedata+salt+cuid+mac+key)
-        console.log(imagedata)
-        wx.uploadFile({
-          url: 'https://fanyi-api.baidu.com/api/trans/sdk/picture',
-          filePath: res.tempFiles[0].tempFilePath,
-          name: '1.jpg',
-          formData: {
-            image,
-            from:'zh',
-            to:'en',
-            appid,
-            salt,
-            cuid,
-            mac,
-            version:3,
-            sign
-          },
-          success(res) {
-            console.log('上传成功',res.data)
-          },
-          fail(res) {
-            console.log(res)
-          },
-          complete() {
-            wx.hideLoading()
-          }
-        })
-      }
     })
   },
 })
